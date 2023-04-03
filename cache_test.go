7be5cc73c0a7b45c7f87d7699149a9447de347a3
James Trotter <james@honestbank.com>
2023-04-03 10:40:04 +0700

    tests

--- cache_test.go
+++ cache_test.go
@@ -236,3 +236,35 @@ func TestCacheFailSet(t *testing.T) {
 		a.Nil(result)
 	})
 }
+
+func TestRememberWait(t *testing.T) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: ":6379", // We connect to host redis, thats what the hostname of the redis service is set to in the docker-compose
+		DB:   0,
+	})
+
+	cache := cache_lib.NewCache[Response](redisClient, &cache_lib.CacheOptions{
+		SubscriptionTimeout: 1 * time.Second,
+		UnsubscribeAndClose: true,
+	})
+
+	t.Run("single cache", func(t *testing.T) {
+		a := assert.New(t)
+		response := Response{Result: true}
+		//responseString, _ := json.Marshal(response)
+		go func() {
+			_, _ = cache.RememberBlocking(context.Background(), func(ctx context.Context) (*Response, error) {
+				time.Sleep(8 * time.Second)
+				return &response, nil
+			}, func(ctx context.Context, data *Response) {}, "data2", 1*time.Second)
+		}()
+		_, err := cache.RememberBlocking(context.Background(), func(ctx context.Context) (*Response, error) {
+			time.Sleep(5 * time.Second)
+
+			return &response, nil
+		}, func(ctx context.Context, data *Response) {}, "data2", 1*time.Second)
+
+		a.Error(err)
+		a.Equal("error reading from pub/sub", err.Error())
+	})
+}
